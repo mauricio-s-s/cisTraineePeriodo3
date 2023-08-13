511e9d4fb97f8b55df10498924ef3ba9ad2f8e70
mauricio-s-s <139659808+mauricio-s-s@users.noreply.github.com>
2023-08-12 22:58:40 -0300

    Add files via upload

--- mlproject3 - FINAL.py
+++ mlproject3 - FINAL.py
@@ -12,6 +12,8 @@ import math
 import tensorflow as tf
 
 
+import matplotlib.pyplot as plt
+
 
 #funções a serem utilizadas
 def accuracy(cm):
@@ -28,6 +30,26 @@ def accuracy(cm):
        
        print()
    return gen_acc
+
+def dropOutMatrix(p,shape):
+    #esta matrix a ser gerada deve anular colunas inteiras aleatoriamente das matrizes de coeficientes
+    dom = np.random.binomial(1,1-p,(1,shape[1])) #matrix com 1-p probabilidade de haver 1 e logo p probabilidade de ser 0
+    dom = np.repeat(dom, shape[0],axis = 0)
+    return dom
+
+
+def mse(a,y):
+    return (a-y)**2
+def deriv_mse(a,y):
+    return 2*(a-y)
+
+def crossEntropyLF(a,y):
+    return y*np.log(a) + (1-y)*np.log(1-a)
+
+def deriv_crossEntropyLF(a,y):
+    return np.divide((a-y),(a**2-a))
+
+
 
 ####### Funções de Ativação ###############
 def relU(x):
@@ -49,14 +71,35 @@ def daf(x):
     return drelU(x)
 
 
+def Cf(a,y,op):
+    if op == 'mse':
+        return mse(a,y)
+    elif op == 'crossEntropy':
+        return crossEntropyLF(a,y)
+    else:
+        a = input('erro: não foi informado o tipo de função')
+
+def dc_da(a,y,op):
+    if op == 'mse':
+        return deriv_mse(a,y)
+    elif op == 'crossEntropy':
+        return deriv_crossEntropyLF(a,y)
+    else:
+        a = input('erro: não foi informado o tipo de função')
+
 os.chdir(r'I:\Meu Drive\engEletrica\IEEE\3periodo')
 dbpath = os.getcwd()+'\\creditcard.csv'
 data = pd.read_csv(dbpath)
 
 
+
 #Preprocessing 
+
+
 data.dtypes
 
+#checar se há valores nulos
+data.isna().sum()
 
 #desconsiderar coluna "Time"
 data = data.drop("Time",axis = 'columns')
@@ -101,16 +144,19 @@ X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_siz
 nFeatures = X_train.shape[1]
 hidden_layer_size = (100)
 
-mBatchSize = 40
+mBatchSize = 500
 epochs = 10
-learnRate = 0.5
-
+learnRate = 0.05
+dropOut = True
+dropOut_p = 0.4
 
 coef0 = np.random.randn(hidden_layer_size,nFeatures)
 coef1 = np.random.randn(1,hidden_layer_size)#
 
 biasw0 = np.random.randn(hidden_layer_size,1)
 biasw1 = np.random.randn(1,1)
+
+losses = []
 
 for i in range(epochs):
     #count = 0
@@ -129,24 +175,43 @@ for i in range(epochs):
         
         bsize = x.shape[1]
         
+        #coeficientes a serem usados na instância de treino
+        if not dropOut:
+            coef0t = coef0
+            coef1t = coef1
+            biasw0t = biasw0
+            biasw1t = biasw1
+        else:
+            coef0t = coef0      * dropOutMatrix(dropOut_p,coef0.shape)
+            coef1t = coef1      * dropOutMatrix(dropOut_p,coef1.shape)
+            biasw0t = biasw0    * dropOutMatrix(dropOut_p,biasw0.shape)
+            biasw1t = biasw1    * dropOutMatrix(dropOut_p,biasw1.shape)
+        
         # Feedforward
-        z0 = np.dot(coef0,x)+biasw0
+        z0 = np.dot(coef0t,x)+biasw0
         activation0 = af(z0)
         
-        z1 = np.dot(coef1,activation0)+biasw1
+        
+        z1 = np.dot(coef1t,activation0)+biasw1
         activation1 = af(z1)
         
+        #calcular erro
+        loss = np.mean(Cf(activation1,y,'mse'))
+        losses.append(loss)
         
         #Backward pass
-        delta1 = 2*(activation1-y) * daf(z1)
+        #dc_da1 = 2*(activation1-y)   #Considerando loss function: média quadrática
+        dc_da1 = dc_da(activation1,y,'mse') 
+        delta1 = dc_da1 * daf(z1)
         grad_biasw1 = delta1  #nabla_b[-1] 
         grad_coef1 = np.dot(delta1,activation0.transpose())/bsize #nabla_w[-1]
         
         
         sp0 = daf(z0)
-        delta0 = np.dot(coef1.transpose(), delta1) * sp0
+        delta0 = np.dot(coef1t.transpose(), delta1) * sp0
         grad_biasw0 = delta0
         grad_coef0 = np.dot(delta0,x.T)/bsize
+        
         
         
         coef0 = coef0 - learnRate*grad_coef0
@@ -161,8 +226,10 @@ for i in range(epochs):
     a0 = af(z0)
     z1 = np.dot(coef1,a0)+biasw1
     a1 = af(z1)
-    loss = np.mean((a1-y_valid.to_numpy().T.reshape(a1.shape))**2)
+    yadj = y_valid.to_numpy().T.reshape(a1.shape)
+    loss = np.mean((a1-yadj)**2)
     print('overall loss: ', loss)
+    print('crossEntropy Loss:', np.mean(Cf(a1,yadj,'crossEntropy')))
     
     #Acurácia nos dados de teste
     #selecionar dados:
@@ -182,4 +249,7 @@ for i in range(epochs):
     print("Classification Report: \n", classification_report(y.reshape(1,-1)[0], test_pred[0]))
 
     
+losses
+
+plt.plot(range(len(losses)), losses)
 
